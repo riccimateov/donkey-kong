49e1602a6ce38c4ffbb2499edf37fc93afa9053c
unknown <Mateo>
2023-05-07 00:30:30 -0300

    Source Upload

--- Donkey_Kong_private.h
+++ Donkey_Kong_private.h
@@ -5,19 +5,19 @@
 #define DONKEY_KONG_PRIVATE_H
 
 /* VERSION DEFINITIONS */
-#define VER_STRING	"1.0.0.1751"
+#define VER_STRING	"1.0.0.1667"
 #define VER_MAJOR	1
 #define VER_MINOR	0
 #define VER_RELEASE	0
-#define VER_BUILD	1751
+#define VER_BUILD	1667
 #define COMPANY_NAME	""
-#define FILE_VERSION	"1.0.0.1751"
+#define FILE_VERSION	"1.0.0.1667"
 #define FILE_DESCRIPTION	"Developed using the Dev-C++ IDE"
 #define INTERNAL_NAME	""
 #define LEGAL_COPYRIGHT	""
 #define LEGAL_TRADEMARKS	""
 #define ORIGINAL_FILENAME	""
 #define PRODUCT_NAME	""
-#define PRODUCT_VERSION	"1.0.0.1751"
+#define PRODUCT_VERSION	"1.0.0.1667"
 
 #endif /*DONKEY_KONG_PRIVATE_H*/
